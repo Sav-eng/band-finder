207498e054aa5df8ac80b954e9e4533fe7d3ded1
Siratish <chert31@hotmail.com>
2020-11-11 01:06:08 +0700

    add musician,instrument,musicGenre foreignkey

--- foreignKeyAdsAndSend.sql
+++ foreignKeyAdsAndSend.sql
@@ -2,7 +2,11 @@ ALTER TABLE advertisement ADD CONSTRAINT fk_advertisement_userId FOREIGN KEY (us
 ALTER TABLE send ADD CONSTRAINT fk_send_userIdSend FOREIGN KEY (userIdSend) REFERENCES user(userId);
 ALTER TABLE send ADD CONSTRAINT fk_send_userIdReceieve FOREIGN KEY (userIdReceieve) REFERENCES user(userId);
 ALTER TABLE send ADD CONSTRAINT fk_send_chatId FOREIGN KEY (chatId) REFERENCES chat(chatId);
-ALTER TABLE send ADD CONSTRAINT fk_admin_userId FOREIGN KEY (userId) REFERENCES user(userId);
-ALTER TABLE send ADD CONSTRAINT fk_music_associate_userId FOREIGN KEY (userId) REFERENCES user(userId);
-ALTER TABLE send ADD CONSTRAINT fk_band_manager_userId FOREIGN KEY (userId) REFERENCES music_associate(userId);
-ALTER TABLE send ADD CONSTRAINT fk_band_userId FOREIGN KEY (userId) REFERENCES band_manager(userId);
+ALTER TABLE admin ADD CONSTRAINT fk_admin_userId FOREIGN KEY (userId) REFERENCES user(userId);
+ALTER TABLE music_associate ADD CONSTRAINT fk_music_associate_userId FOREIGN KEY (userId) REFERENCES user(userId);
+ALTER TABLE band_manager ADD CONSTRAINT fk_band_manager_userId FOREIGN KEY (userId) REFERENCES music_associate(userId);
+ALTER TABLE band ADD CONSTRAINT fk_band_userId FOREIGN KEY (userId) REFERENCES band_manager(userId);
+ALTER TABLE musician ADD CONSTRAINT fk_musician_userId FOREIGN KEY (userId) REFERENCES music_associate(userId);
+ALTER TABLE musician ADD CONSTRAINT fk_musician_performerId FOREIGN KEY (performerId) REFERENCES performer(performerId);
+ALTER TABLE musician_instrument ADD CONSTRAINT fk_musician_instrument_userId FOREIGN KEY (userId) REFERENCES musician(userId);
+ALTER TABLE musician_musicGenre ADD CONSTRAINT fk_musician_musicGenre_userId FOREIGN KEY (userId) REFERENCES musician(userId);
