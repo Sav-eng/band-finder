51a9a177cb921d67b3760c33de7806df950f9168
FinalJTth <FinalJTth_31@hotmail.com>
2020-11-12 00:10:41 +0700

    Fix mismatch attribute name.

--- createTable/createReview.sql
+++ createTable/createReview.sql
@@ -2,7 +2,7 @@ CREATE TABLE review(
   reviewId INT NOT NULL AUTO_INCREMENT,
   performerId INT NOT NULL,
   userId INT NOT NULL,
-  voteNumer INT NOT NULL,
+  voteNumber INT NOT NULL,
   reviewMessage TEXT,
   score TINYINT NOT NULL,
   type VARCHAR(30) NOT NULL,
