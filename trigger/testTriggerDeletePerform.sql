af465c5151a098cf1645836b433b560143b62bac
Narawit Rakket <narawitrakket@hotmail.com>
2020-11-11 23:19:59 +0700

    add test for TriggerDeletePerform

--- trigger/testTriggerDeletePerform.sql
+++ trigger/testTriggerDeletePerform.sql
@@ -1,7 +1,5 @@
-INSERT INTO user VALUES
-(999, 'Team:)@chula.co.th', 'ProfilePicture', 'Password', 'creditCardNumber', 'telephoneNumber', FALSE);
-INSERT INTO transaction VALUES
-(999, 10, 'Tamoad Bank', NOW(), 'Payment', '1');
-DELETE FROM transaction WHERE transactionID = 999;
-DELETE FROM user WHERE userId = 999;
-SELECT * FROM deletedTransaction
+INSERT INTO perform VALUES
+(999, 1, 2, 10000);
+
+DELETE FROM perform WHERE performID = 999;
+SELECT * FROM deletedPerform;              
