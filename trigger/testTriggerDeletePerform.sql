2288c7dd70c756f29c20d1befd53952e893e5045
Narawit Rakket <narawitrakket@hotmail.com>
2020-11-11 23:08:22 +0700

    edit trigger

---
+++ trigger/testTriggerDeletePerform.sql
@@ -0,0 +1,7 @@
+INSERT INTO user VALUES
+(999, 'Team:)@chula.co.th', 'ProfilePicture', 'Password', 'creditCardNumber', 'telephoneNumber', FALSE);
+INSERT INTO transaction VALUES
+(999, 10, 'Tamoad Bank', NOW(), 'Payment', '1');
+DELETE FROM transaction WHERE transactionID = 999;
+DELETE FROM user WHERE userId = 999;
+SELECT * FROM deletedTransaction
