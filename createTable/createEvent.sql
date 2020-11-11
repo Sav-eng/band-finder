a5e5cf096c6b927ca51510758e3eae82bc38200f
Theme <5batman400@gmail.com>
2020-11-11 23:24:41 +0700

    nid noi

--- createTable/createEvent.sql
+++ createTable/createEvent.sql
@@ -4,7 +4,7 @@ CREATE TABLE event(
   name VARCHAR(20) NOT NULL,
   location VARCHAR(100) NOT NULL,
   ticketPrice FLOAT(24),
-  description TEXT NOT NULL,
+  description TEXT,
   date DATETIME NOT NULL,
   PRIMARY KEY (eventId),
   FOREIGN KEY (userId) REFERENCE organizer(userId) ON DELETE CASCADE
