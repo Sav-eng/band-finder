03305218e57183b9d949be76e6845409eb122171
FinalJTth <FinalJTth_31@hotmail.com>
2020-11-12 00:37:44 +0700

    Fix storedProcedure to correctly show most recent event.

--- storedProcedure/GetPerformerMostRecentEvent.sql
+++ storedProcedure/GetPerformerMostRecentEvent.sql
@@ -7,7 +7,7 @@ BEGIN
 	SELECT E.name, E.date, E.location
 	FROM event E, perform PF
   WHERE PF.performerId = performerId and E.eventId = PF.eventId
-  ORDER BY E.date ASC
+  ORDER BY E.date DESC
   LIMIT 1;
 END$$
 
