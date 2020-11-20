2773554d92f3c48894e724f81907120442069b6c
Thanathai Lertpetchpun <save-000@hotmail.com>
2020-11-20 15:47:04 +0700

    edit time format entity chat

--- createTable/createTransaction.sql
+++ createTable/createTransaction.sql
@@ -2,7 +2,7 @@ CREATE TABLE transaction(
   transactionId INT NOT NULL AUTO_INCREMENT,
   refNumber INT NOT NULL,
   bank VARCHAR(20) NOT NULL,
-  date DATETIME NOT NULL,
+  dateTime DATETIME NOT NULL,
   type VARCHAR(20) NOT NULL,
   amount FLOAT(24) NOT NULL,
   PRIMARY KEY (transactionId)
