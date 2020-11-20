c70d027764f10abc6707f80c17027a8339218743
Narawit Rakket <narawitrakket@hotmail.com>
2020-11-20 18:37:55 +0700

    fix typo fk_send_userIdReceive

--- addForeignKey/alterForeignKey.sql
+++ addForeignKey/alterForeignKey.sql
@@ -1,6 +1,6 @@
 ALTER TABLE advertisement ADD CONSTRAINT fk_advertisement_userId FOREIGN KEY (userId) REFERENCES user(userId);
 ALTER TABLE send ADD CONSTRAINT fk_send_userIdSend FOREIGN KEY (userIdSend) REFERENCES user(userId);
-ALTER TABLE send ADD CONSTRAINT fk_send_userIdReceieve FOREIGN KEY (userIdReceieve) REFERENCES user(userId);
+ALTER TABLE send ADD CONSTRAINT fk_send_userIdReceive FOREIGN KEY (userIdReceive) REFERENCES user(userId);
 ALTER TABLE send ADD CONSTRAINT fk_send_chatId FOREIGN KEY (chatId) REFERENCES chat(chatId) ON DELETE CASCADE;
 ALTER TABLE admin ADD CONSTRAINT fk_admin_userId FOREIGN KEY (userId) REFERENCES user(userId);
 ALTER TABLE musicAssociate ADD CONSTRAINT fk_musicAssociate_userId FOREIGN KEY (userId) REFERENCES user(userId);
