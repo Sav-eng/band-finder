aa5559d462a4ac7f809896d6f70de9d4f7a20d11
Borvornwit <your_email@example.competebest47@gmail.com>
2020-11-21 22:30:51 +0700

    fix natural join

--- query/eventSort.sql
+++ query/eventSort.sql
@@ -4,12 +4,12 @@ FROM (
 		userId, 
 		name, 
 		score 
-	FROM user
-		NATURAL JOIN organizer
+	FROM user, organizer
 		LEFT OUTER JOIN review
+	WHERE user.userId = organizer.userId
 	GROUP BY userId
-	ORDER BY sum(score) DESC)
-	NATURAL JOIN advertisement
-WHERE CURRENT_DATE BETWEEN startDate AND endDate
+	ORDER BY sum(score) DESC) as mapped,
+	advertisement
+WHERE mapped.userId = advertisement.userId AND CURRENT_DATE BETWEEN startDate AND endDate
 GROUP BY userId
 ORDER BY price DESC;
