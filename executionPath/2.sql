7542deb7b6974912db3d18ec52cedf6816207856
Thanathai Lertpetchpun <save-000@hotmail.com>
2020-11-22 02:22:21 +0700

    edit execution path

--- executionPath/2.sql
+++ executionPath/2.sql
@@ -1,9 +1,9 @@
 -- get firstname lastname of member in bandId 2
 SELECT firstname, lastname 
-FROM musicAssociate
-WHERE userId IN 
+FROM musicAssociate C
+WHERE C.userId IN 
 (
-	SELECT userId 
+	SELECT A.userId 
 	FROM musician A, joint B
 	WHERE A.userId = B.userId AND
 	B.bandId = 2
