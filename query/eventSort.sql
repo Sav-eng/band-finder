35e8f034e711da7205234b01316608b2350f5290
Borvornwit <your_email@example.competebest47@gmail.com>
2020-11-21 22:13:27 +0700

    add group by userId

--- query/eventSort.sql
+++ query/eventSort.sql
@@ -1,9 +1,15 @@
-SELECT name
-FROM (select userId, name, score 
+SELECT name, score
+FROM (
+	SELECT 
+		userId, 
+		name, 
+		score 
 	FROM user
 		NATURAL JOIN organizer
 		LEFT OUTER JOIN review
+	GROUP BY userId
 	ORDER BY sum(score) DESC)
 	NATURAL JOIN advertisement
 WHERE CURRENT_DATE BETWEEN startDate AND endDate
+GROUP BY userId
 ORDER BY price DESC;
