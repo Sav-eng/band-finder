2cd7cfa21073a9f6d6c2908ed1517eb6347e0d3b
FinalJTth <FinalJTth_31@hotmail.com>
2020-11-22 13:48:48 +0700

    Change event location to unique type so it's able to add foreign key for table location

--- createTable/createEvent.sql
+++ createTable/createEvent.sql
@@ -2,7 +2,7 @@ CREATE TABLE event(
   eventId INT NOT NULL AUTO_INCREMENT,
   userId INT NOT NULL,
   name VARCHAR(20) NOT NULL,
-  location VARCHAR(100) NOT NULL,
+  location VARCHAR(100) NOT NULL UNIQUE,
   ticketPrice FLOAT(24),
   description TEXT,
   dateTime DATETIME NOT NULL,
